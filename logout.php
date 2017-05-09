f3ae8db6471c3321c1283ad74aa1f8f5160ade34
roiik16 <roik1234@gmail.com>
2017-05-09 14:02:36 +0200

    CLEANED CODE IS HERE
    THE FIRST COMMIT IS WRONG. THIS IS THE CLEANED CODE

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'functions.php';
-
-    foreach ($_COOKIE as $key => $value) {
-        setcookie ($key, '', time () - 60);
-    }
-
-    redirect ("index.php");
-?>
--- userdata.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'functions.php';
-
-    var_dump (load_note_from_disk (1));
-?>
